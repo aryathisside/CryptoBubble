3bd1525dbb308eaaf7346d790a2bdafbf8c3d166
Moushumi Maitra <moushumi@netcomlearning.com>
2025-03-05 00:12:26 +0530

    new ui

--- src/cypto-academy/Components/Buttons/Logout.jsx
+++ src/cypto-academy/Components/Buttons/Logout.jsx
@@ -28,9 +28,9 @@ const Logout = () => {
       <button
         type="button"
         onClick={logoutHandler}
-        className="text-white my-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-semibold rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 font-text"
+        className="text-[#FF3333] my-2 font-semibold rounded-lg text-md px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 font-text"
       >
-        Logout
+        Log Out
       </button>
     </>
   );
