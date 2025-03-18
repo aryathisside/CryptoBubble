11eb1fd28cad65722b4ec0f644ec1aaf34119913
Moushumi Maitra <moushumi@netcomlearning.com>
2025-03-19 00:56:07 +0530

    fixes

--- src/cypto-academy/Components/CoinsTable.jsx
+++ src/cypto-academy/Components/CoinsTable.jsx
@@ -12,9 +12,9 @@ import Loader from './Loader';
 import DynamicPagination from '../../components/common/Pagination';
 import BuyCoins from './BuyCoins';
 import MiniWatchlist from './MiniWatchlist';
-import { MdOutlineStarBorder } from "react-icons/md";
-import percentIcon from "../Assets/svg/percent-icon.svg";
-import priceIcon from "../Assets/svg/price-icon.svg";
+import { MdOutlineStarBorder } from 'react-icons/md';
+import percentIcon from '../Assets/svg/percent-icon.svg';
+import priceIcon from '../Assets/svg/price-icon.svg';
 
 const CoinsTable = () => {
   const navigate = useNavigate();
@@ -65,7 +65,6 @@ const CoinsTable = () => {
     return marketCap;
   };
 
-
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = data?.slice(indexOfFirstItem, indexOfLastItem);
@@ -74,66 +73,70 @@ const CoinsTable = () => {
 
   return (
     <>
-    <div className="z-10 w-full md:w-[72%]">
-      {(isLoading || fetchGlobalCryptoLoading) && <Loader />}
-      {error && <ErrorToast message="Something Went Wrong!" ref={toastRef} />}
-      <div className="bg-[#171A24] py-6 px-4 rounded-[12px] mx-3 md:mt-6 sm:mt-4 mb-2 md:mb-4">
-        <div className="md:flex justify-between border-b-2 border-[#2A2E36]">
-          <div>
-            <p className="text-white font-bold text-xl md:text-2xl font-title lg:mt-0 mb-2 ml-3">Market</p>
-            <p className="text-[#A9A9A9] text-sm  font-title lg:mt-0 mb-4 ml-3">Real time market insights and seamless trading simulation</p>
-          </div>
-          <div className="md:px-4"  onClick={() => navigate('/papertrade/app/search')}>
-            <label for="table-search" className="sr-only">
-              Search
-            </label>
-            <div className="relative mt-1">
-              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fillRule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clipRule="evenodd"></path>
-                </svg>
+      <div className="z-10 w-full md:w-[72%]">
+        {(isLoading || fetchGlobalCryptoLoading) && <Loader />}
+        {error && <ErrorToast message="Something Went Wrong!" ref={toastRef} />}
+        <div className="bg-[#171A24] py-6 px-4 rounded-[12px] mx-3 md:mt-6 sm:mt-4 mb-2 md:mb-4">
+          <div className="md:flex justify-between border-b-2 border-[#2A2E36]">
+            <div>
+              <p className="text-white font-bold text-xl md:text-2xl font-title lg:mt-0 mb-2 ml-3">Market</p>
+              <p className="text-[#A9A9A9] text-sm  font-title lg:mt-0 mb-4 ml-3">Real time market insights and seamless trading simulation</p>
+            </div>
+            <div className="md:px-4" onClick={() => navigate('/papertrade/app/search')}>
+              <label for="table-search" className="sr-only">
+                Search
+              </label>
+              <div className="relative mt-1">
+                <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                  <svg
+                    className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      fillRule="evenodd"
+                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                      clipRule="evenodd"></path>
+                  </svg>
+                </div>
+                <input
+                  type="text"
+                  id="table-search"
+                  className="w-full mb-4 sm:w-[auto] lg:w-[300px] text-sm rounded-lg block pl-10 p-2.5 bg-[#171A24] border-2 border-[#2A2E36] placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                  placeholder="Search Crypto here..."
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                />
               </div>
-              <input
-                type="text"
-                id="table-search"
-               className="w-full mb-4 sm:w-[auto] lg:w-[300px] text-sm rounded-lg block pl-10 p-2.5 bg-[#171A24] border-2 border-[#2A2E36] placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Search Crypto here..."
-                onChange={(e) => {
-                  setSearch(e.target.value);
-                }}
-              />
             </div>
           </div>
+
+          <div className=" flex gap-3 md:gap-8 rounded-box justify-between w-screen w-full overflow-auto max-w-full mt-4">
+            {slicedData.length > 0 ? (
+              slicedData.map((coins, index) => (
+                <div key={coins.id || index} className="w-full flex-1 bg-[#080808] p-3 rounded-lg">
+                  <div className="flex items-center gap-1">
+                    <img src={coins.image} alt={coins.name || 'cryptocurrency'} className="w-8 h-8 object-contain" />
+                    <p className=" truncate text-sm text-white font-semibold break-words">{coins.name}</p>
+                  </div>
+                  <div className="text-white font-semibold pt-3">$ {coins.current_price}</div>
+                  <div className="flex items-center text-sm justify-start gap-1 ml-auto md:ml-0 ">
+                    <img src={percentIcon} alt="percent" />
+                    <p className={` ${coins?.price_change_percentage_24h >= 0 ? 'text-green-400' : 'text-red-400'} font-semibold text-sm`}>
+                      {coins?.price_change_percentage_24h >= 0 && '+'}
+                      {coins?.price_change_percentage_24h?.toFixed(2)}%
+                    </p>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p className="text-white">No data available</p>
+            )}
+          </div>
         </div>
 
-        <div className=" flex gap-3 md:gap-8 rounded-box justify-between w-screen w-full overflow-auto max-w-full mt-4">
-          {slicedData.length > 0 ? (
-            slicedData.map((coins, index) => (
-              <div key={coins.id || index} className="w-full flex-1 bg-[#080808] p-3 rounded-lg">
-                <div className="flex items-center gap-1">
-                  <img src={coins.image} alt={coins.name || 'cryptocurrency'} className="w-8 h-8 object-contain" />
-                  <p className=" truncate text-sm text-white font-semibold break-words">{coins.name}</p>
-                </div>
-                <div className="text-white font-semibold pt-3">$ {coins.current_price}</div>
-                <div className="flex items-center text-sm justify-start gap-1 ml-auto md:ml-0 ">
-                  <img src={percentIcon} alt='percent' />
-                  <p className={` ${coins?.price_change_percentage_24h >= 0 ? 'text-green-400' : 'text-red-400'} font-semibold text-sm`}>
-                    {coins?.price_change_percentage_24h >= 0 && '+'}
-                    {coins?.price_change_percentage_24h?.toFixed(2)}%
-                  </p>
-                </div>
-              </div>
-            ))
-          ) : (
-            <p className="text-white">No data available</p>
-          )}
-        </div>
-      </div>
-
-      {/* 
+        {/* 
       {fetchGlobalCryptoSuccess && (
         <div className="no-scrollbar flex flex-wrap p-4 gap-3 md:gap-8 rounded-box justify-between w-screen w-full overflow-auto max-w-full">
           <div className="w-full flex-1">
@@ -190,101 +193,89 @@ const CoinsTable = () => {
           </div>
         </div>
       )} */}
-      {/* more global stats page */}
-      {/* {fetchGlobalCryptoSuccess && (
+        {/* more global stats page */}
+        {/* {fetchGlobalCryptoSuccess && (
         <Link to="/papertrade/app/market/globalStats" className="text-md  font-semibold text-green-400 px-4 underline">
           Show more global stats.
         </Link>
       )} */}
-      {/* coin table */}
-      <div className="bg-[#171A24] py-6 px-4 rounded-[12px] mx-3 md:mb-4 mb-3 sm:mt-3 md:mt-8">
+        {/* coin table */}
+        <div className="bg-[#171A24] py-6 px-4 rounded-[12px] mx-3 md:mb-4 mb-3 sm:mt-3 md:mt-8">
+          <div className=" mb-2">
+            <button className="border-2 border-[#CFA935] text-[#CFA935] p-2 rounded">Market Cap</button>
+          </div>
 
-        <div className="md:px-4 mb-2">
-          <button className="border-2 border-[#CFA935] text-[#CFA935] p-2 rounded">Market Cap</button>
-        </div>
+          <table className="w-full text-white border-collapse font-light mt-4">
+            {/* Table Head */}
+            <thead className="bg-[#2A2E36]">
+              <tr className="text-[#A9A9A9] text-sm font-light">
+                {/* Apply rounded corners based on screen size */}
+                <th className="hidden lg:table-cell py-2 pl-3 lg:rounded-tl-md lg:rounded-bl-md">S.no</th>
+                <th className="text-left p-2">Name</th>
+                <th className="p-2">Price</th>
+                <th className="hidden lg:table-cell text-center p-2">24h Volume</th>
+                <th className="hidden md:table-cell text-center p-2">24h %</th>
+                <th className="hidden lg:table-cell text-center p-2">24h High</th>
+                <th className="hidden lg:table-cell text-center p-2 lg:rounded-tr-md lg:rounded-br-md">24h Low</th>
+          
+              </tr>
+            </thead>
 
-        <ul className="md:px-4 flex flex-col space-y-1 pb-12 font-text text-white p-2">
-          {/* Table Head */}
-          <li className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-7 text-gray-500 py-3 px-1 md:px-5 cursor-pointer bg-[#2A2E36] rounded-lg">
-            <div className="hidden lg:flex justify-start items-center space-x-4 w-12">
-              <p className="text-white pl-4">S.no</p>
-            </div>
-            <div className="flex justify-start items-center space-x-4">
-              <p className="w-28 md:w-40 text-white pl-4">Name</p>
-            </div>
-            <div className="flex items-center justify-center ml-auto md:ml-0 ">
-              <p className=" text-white">Price</p>
-            </div>
-            <div className="hidden lg:flex items-center justify-center ml-auto md:ml-0 ">
-              <p className=" text-white">24h Volume</p>
-            </div>
-            <div className="hidden md:flex items-center justify-center ml-auto md:ml-0 ">
-              <p className=" text-white">24h %</p>
-            </div>
-            <div className="hidden lg:flex items-center justify-center ml-auto md:ml-0 ">
-              <p className=" text-white">24h High</p>
-            </div>
-            <div className="hidden lg:flex items-center justify-center ml-auto md:ml-0 ">
-              <p className="text-white">24h Low</p>
-            </div>
-          </li>
-          {/* coin prices */}
-          {isSuccess &&
-            currentItems?.map((coins, index) => (
-              <li
-                key={index}
-                onClick={() => navigate(`/papertrade/app/coin/${coins.id}`)}
-                className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-7 text-gray-500 py-2 px-1md:px-5 hover:bg-gray-900 rounded-lg cursor-pointer border-gray-800 ">
-                <div className="w-12 hidden lg:flex items-center">
-                  <p className="pl-4 text-white flex items-center"><MdOutlineStarBorder/> {(currentPage - 1) * itemsPerPage + index + 1}</p>
-                </div>
-                <div className="flex items-center space-x-2 ">
-                  <img className="h-7 w-7 md:h-8 md:w-8 object-contain" src={coins.image} alt="cryptocurrency" loading="lazy" />
-                  <div>
-                    <p className=" w-64 truncate text-white font-semibold break-words">{coins.name}</p>
-                    <div className="flex space-x-1">
-                      <p>{`${coins.symbol}/usd`.toUpperCase()}</p>
-                      {/* <p
-                      className={`md:hidden w-24 md:w-40 ${
-                        coins?.price_change_percentage_24h >= 0 ? "text-green-400" : "text-red-400"
-                      } font-semibold`}
-                    >
-                      {coins?.price_change_percentage_24h >= 0 && "+"}
+            {/* Table Body */}
+            <tbody>
+              {isSuccess &&
+                currentItems?.map((coins, index) => (
+                  <tr key={index} onClick={() => navigate(`/papertrade/app/coin/${coins.id}`)} className=" hover:bg-[#080808] cursor-pointer mt-2">
+                    {/* S.no */}
+                    <td className="hidden lg:table-cell p-3">
+                      <div className="flex items-center text-sm">
+                        <MdOutlineStarBorder />
+                        {(currentPage - 1) * itemsPerPage + index + 1}
+                      </div>
+                    </td>
+
+                    {/* Name */}
+                    <td className="flex items-center space-x-2 p-3">
+                      <img className="h-7 w-7 md:h-8 md:w-8 object-contain" src={coins.image} alt="crypto" loading="lazy" />
+                      <div>
+                        <p className="text-white font-semibold truncate text-sm">
+                          {coins.name} <span className="text-[#CFA935] text-[8px] bg-[#00000033] p-1.5"> {coins.symbol.toUpperCase()}</span>
+                        </p>
+                        {/* <p className=' text-sm'>{`$/USD`}</p> */}
+                      </div>
+                    </td>
+
+                    {/* Price */}
+                    <td className="text-center p-3">
+                      <p className="flex items-center text-sm">
+                        ${coins.current_price}
+                        <img src={priceIcon} alt="price" className="w-4 h-4 ml-1" />
+                      </p>
+                    </td>
+
+                    {/* 24h Volume */}
+                    <td className="hidden lg:table-cell  text-sm p-3">${coins.total_volume}</td>
+
+                    {/* 24h % */}
+                    <td
+                      className={`hidden md:table-cell text-center text-sm p-3 ${coins?.price_change_percentage_24h >= 0 ? 'text-green-400' : 'text-red-400'}`}>
+                      {coins?.price_change_percentage_24h >= 0 && '+'}
                       {coins?.price_change_percentage_24h?.toFixed(2)}%
-                    </p> */}
-                    </div>
-                  </div>
-                </div>
-                <div className="flex items-center justify-center ml-auto md:ml-0 ">
-                  <p className=" text-white flex gap-1 items-center">
-                    ${coins.current_price}
-                    <img src={priceIcon} alt='price' className='w-4 h-4'/>
-                    <br />
-                    {/* <span className="md:hidden w-28 md:w-40 text-gray-500">MCap: {normalizeMarketCap(coins.market_cap)}</span> */}
-                  </p>
-                </div>
-                <div className="hidden lg:flex items-center justify-center ml-auto md:ml-0 ">
-                  <p className=" text-white ">${coins.total_volume}</p>
-                </div>
-                <div className="hidden md:flex items-center justify-center ml-auto md:ml-0 ">
-                  <p className={` ${coins?.price_change_percentage_24h >= 0 ? 'text-green-400' : 'text-red-400'}`}>
-                    {coins?.price_change_percentage_24h >= 0 && '+'}
-                    {coins?.price_change_percentage_24h?.toFixed(2)}%
-                  </p>
-                </div>
-                <div className="hidden lg:flex items-center justify-center ml-auto md:ml-0 text-white">
-                  <p className="">${coins.high_24h}</p>
-                </div>
-                <div className="hidden lg:flex items-center justify-center ml-auto md:ml-0 text-white">
-                  <p className=" ">${coins.low_24h}</p>
-                </div>
-              </li>
-            ))}
-        </ul>
+                    </td>
 
-        <DynamicPagination totalItems={data?.length} itemsPerPage={itemsPerPage} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-        {/* pagination */}
-        {/* <ReactPaginate
+                    {/* 24h High */}
+                    <td className="hidden lg:table-cell text-center  text-sm p-3">${coins.high_24h}</td>
+
+                    {/* 24h Low */}
+                    <td className="hidden lg:table-cell text-center text-sm p-3">${coins.low_24h}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+
+          <DynamicPagination totalItems={data?.length} itemsPerPage={itemsPerPage} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+          {/* pagination */}
+          {/* <ReactPaginate
         previousLabel={'<'}
         nextLabel={'>'}
         breakLabel={'...'}
@@ -298,14 +289,13 @@ const CoinsTable = () => {
         previousLinkClassName={`block w-8 h-8 leading-8 text-center text-white  bg-green-600 border-green-600 rounded-full`}
         nextLinkClassName={`block w-8 h-8 leading-8 text-center text-white  bg-green-600 border-green-600 rounded-full`}
       /> */}
+        </div>
       </div>
-    </div>
       <div className="w-[28%] mx-3 my-4 hidden md:block">
-     {isSuccess && <BuyCoins data={data} />}
-    <MiniWatchlist />
-
-  </div>
-  </>
+        {isSuccess && <BuyCoins data={data} />}
+        <MiniWatchlist />
+      </div>
+    </>
   );
 };
 
